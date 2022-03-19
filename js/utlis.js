8cf5aa691c0cbc70d3cd97d40b9c29aea804834b
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-03-19 18:03:27 -0300

    Adicionando datatable

---
+++ js/utlis.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#table-protocol").DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json'
+        }
+    });
+});
