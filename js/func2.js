d399fe761c70996c2d507f437bd2acbfc046290c
Guilherme <gui155@hotmail.com>
2022-03-20 14:26:53 -0300

    terminando o monitor

--- js/func2.js
+++ js/func2.js
@@ -8,7 +8,15 @@ btnFilter.addEventListener('click', filter);
 $(document).ready(function() {
     $("#table-protocol").DataTable({
         language: {
-            url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json'
+            url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json',
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#table-analysis").DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json',
         }
     });
 });
